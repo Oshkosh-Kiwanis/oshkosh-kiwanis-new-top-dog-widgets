feca6e98de1ca434137c788eea7736d28da57cf1
Jason Cardinal <jason.cardinal@timiosdevelopment.com>
2021-09-02 18:31:10 -0500

    update layout for leaderboards

--- src/components/new-top-dog-contest-leaderboard/new-top-dog-contest-leaderboard.tsx
+++ src/components/new-top-dog-contest-leaderboard/new-top-dog-contest-leaderboard.tsx
@@ -36,13 +36,13 @@ export class NewTopDogContestLeaderboard {
         this.dogs?.map(dog => 
           <div class="dog">
             <a href={dog.page} title={"Page for " + dog.dog} style={{ backgroundImage: `url(${dog.picture})`, backgroundSize: "cover", backgroundPosition: "center center" }}>
-              <div class="info">
-                <div class="name"><span>{dog.dog}</span></div>
-                <div class="votes"><span>{dog.votes} Votes</span></div>
-              </div>
             </a>
+            <div class="info">
+              <div class="name"><span>{dog.dog}</span></div>
+              <div class="votes"><span>{dog.votes} Votes</span></div>
+            </div>
             <div class="actions">
-              <a href={dog.page} target="_blank">Vote For {dog.dog}</a>
+              <a class="et_pb_button et_pb_button_2 et_pb_bg_layout_light" href={dog.page} target="_blank">Vote For {dog.dog}</a>
             </div>
           </div>
         )
